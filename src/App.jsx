70be115f9d2ab794b3378dc07b9b009153b041e2
Rabiul Hossen <rhsimon69@gmail.com>
2023-07-13 00:08:03 +0600

    initial setup for reuse

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Button from './Components/Button'
+
+function App() {
+  
+
+  return (
+    <>
+    <Button name="hello from b"/>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Button.jsx
@@ -0,0 +1,15 @@
+const Button = ({name}) => {
+
+     const styles ={
+          colour: "red",
+          background: "green",
+          padding: "10px"
+     }
+   return (
+      <div>
+         {name}
+      </div>
+   );
+};
+
+export default Button;
